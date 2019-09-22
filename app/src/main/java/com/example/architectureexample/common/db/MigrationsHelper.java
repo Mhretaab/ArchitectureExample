59761ce2323eb613c70c6a59eed0d8e181b62ef9
Mhretaab <ermu96@gmail.com>
2019-09-22 18:02:08 +0300

    mig script for creating note_detail view

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -14,8 +14,8 @@ public final class MigrationsHelper {
             database.execSQL("CREATE TABLE `note` (`title` TEXT, `description` TEXT, `priority` INTEGER NOT NULL, `user_id` INTEGER, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
             database.execSQL("INSERT INTO `note`(`title`, `description`, `priority`, `id`, `uuid`, `date_created`, `date_updated`)\n" +
                     "SELECT `title`, `description`, `priority`, `id`, `uuid`, 1569163537, 1569163537 \n" +
-                    "FROM note_table");
-            database.execSQL("DROP TABLE `note_old`");
+                    "FROM `note_table`");
+            database.execSQL("DROP TABLE `note_table`");
 
             database.execSQL("CREATE INDEX `index_note_title` ON `note` (`title`)");
             database.execSQL("CREATE INDEX `index_note_created_by_user_id` ON `note` (`created_by_user_id`)");
