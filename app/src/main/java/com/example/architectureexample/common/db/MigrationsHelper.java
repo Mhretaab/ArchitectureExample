92418a0f2b63a65448281a8646c2fa54261ea9a0
Mhretaab <ermu96@gmail.com>
2019-09-22 16:33:50 +0300

    fixed mig script

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -8,15 +8,17 @@ public final class MigrationsHelper {
     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE note_table RENAME TO note");
+
+            database.execSQL("CREATE TABLE `user` (`first_name` TEXT, `last_name` TEXT, `email` TEXT, `password` TEXT, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, `street` TEXT, `state` TEXT, `city` TEXT, `post_code` INTEGER, FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
+
             database.execSQL("ALTER TABLE note ADD COLUMN created_by_user_id LONG REFERENCES user(id)");
             database.execSQL("ALTER TABLE note ADD COLUMN updated_by_user_id LONG REFERENCES user(id)");
-            database.execSQL("ALTER TABLE user ADD COLUMN created_by_user_id LONG REFERENCES user(id)");
-            database.execSQL("ALTER TABLE user ADD COLUMN updated_by_user_id LONG REFERENCES user(id)");
 
-            database.execSQL("CREATE INDEX IF NOT EXISTS index_note_created_by_user_id ON note (created_by_user_id)");
-            database.execSQL("CREATE INDEX IF NOT EXISTS index_note_updated_by_user_id ON note (updated_by_user_id)");
-            database.execSQL("CREATE INDEX IF NOT EXISTS index_user_created_by_user_id ON user (created_by_user_id)");
-            database.execSQL("CREATE INDEX IF NOT EXISTS index_user_updated_by_user_id ON user (updated_by_user_id)");
+            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_created_by_user_id ON note (created_by_user_id)");
+            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_updated_by_user_id ON note (updated_by_user_id)");
+            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_user_created_by_user_id ON user (created_by_user_id)");
+            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_user_updated_by_user_id ON user (updated_by_user_id)");
         }
     };
 
