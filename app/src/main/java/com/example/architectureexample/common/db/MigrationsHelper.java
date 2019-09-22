41d62e0a6695cb469e39c03932fceb9fb12d95cf
Mhretaab <ermu96@gmail.com>
2019-09-22 16:55:45 +0300

    changed LONG to INTEGER

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -12,10 +12,10 @@ public final class MigrationsHelper {
 
             database.execSQL("CREATE TABLE `user` (`first_name` TEXT, `last_name` TEXT, `email` TEXT, `password` TEXT, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, `street` TEXT, `state` TEXT, `city` TEXT, `post_code` INTEGER, FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
 
-            database.execSQL("ALTER TABLE note ADD COLUMN date_created LONG");
-            database.execSQL("ALTER TABLE note ADD COLUMN date_updated LONG");
-            database.execSQL("ALTER TABLE note ADD COLUMN created_by_user_id LONG REFERENCES user(id)");
-            database.execSQL("ALTER TABLE note ADD COLUMN updated_by_user_id LONG REFERENCES user(id)");
+            database.execSQL("ALTER TABLE note ADD COLUMN date_created INTEGER");
+            database.execSQL("ALTER TABLE note ADD COLUMN date_updated INTEGER");
+            database.execSQL("ALTER TABLE note ADD COLUMN created_by_user_id INTEGER REFERENCES user(id)");
+            database.execSQL("ALTER TABLE note ADD COLUMN updated_by_user_id INTEGER REFERENCES user(id)");
 
             database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_created_by_user_id ON note (created_by_user_id)");
             database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_updated_by_user_id ON note (updated_by_user_id)");
