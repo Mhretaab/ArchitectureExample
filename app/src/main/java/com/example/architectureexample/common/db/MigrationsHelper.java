8682c65eaf9ff90f49bece48128f19aa8212dd87
Mhretaab <ermu96@gmail.com>
2019-09-22 17:32:17 +0300

    added mig scripts

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -8,19 +8,29 @@ public final class MigrationsHelper {
     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
+
+            database.beginTransaction();
             database.execSQL("ALTER TABLE note_table RENAME TO note");
 
             database.execSQL("CREATE TABLE `user` (`first_name` TEXT, `last_name` TEXT, `email` TEXT, `password` TEXT, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, `street` TEXT, `state` TEXT, `city` TEXT, `post_code` INTEGER, FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
 
-            database.execSQL("ALTER TABLE note ADD COLUMN date_created INTEGER");
-            database.execSQL("ALTER TABLE note ADD COLUMN date_updated INTEGER");
-            database.execSQL("ALTER TABLE note ADD COLUMN created_by_user_id INTEGER REFERENCES user(id)");
-            database.execSQL("ALTER TABLE note ADD COLUMN updated_by_user_id INTEGER REFERENCES user(id)");
+            database.execSQL("ALTER TABLE `note` RENAME TO `note_old`");
+            database.execSQL("CREATE TABLE `note` (`title` TEXT, `description` TEXT, `priority` INTEGER NOT NULL, `user_id` INTEGER, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
+            database.execSQL("INSERT INTO `note`(`title`, `description`, `priority`, `id`, `uuid`)\n" +
+                    "SELECT `title`, `description`, `priority`, `id`, `uuid` \n" +
+                    "FROM note_old");
+            database.execSQL("DROP TABLE `note_old`");
 
-            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_created_by_user_id ON note (created_by_user_id)");
-            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_note_updated_by_user_id ON note (updated_by_user_id)");
-            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_user_created_by_user_id ON user (created_by_user_id)");
-            database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_user_updated_by_user_id ON user (updated_by_user_id)");
+            database.execSQL("CREATE INDEX `index_note_title` ON `note` (`title`)");
+            database.execSQL("CREATE INDEX `index_note_created_by_user_id` ON `note` (`created_by_user_id`)");
+            database.execSQL("CREATE INDEX `index_note_updated_by_user_id` ON `note` (`updated_by_user_id`)");
+            database.execSQL("CREATE INDEX `index_note_user_id` ON `note` (`user_id`)");
+            database.execSQL("CREATE UNIQUE INDEX `index_note_uuid` ON `note` (`uuid`)");
+            database.execSQL("CREATE INDEX `index_user_created_by_user_id` ON `user` (`created_by_user_id`)");
+            database.execSQL("CREATE UNIQUE INDEX `index_user_email` ON `user` (`email`)");
+            database.execSQL("CREATE INDEX `index_user_updated_by_user_id` ON `user` (`updated_by_user_id`)");
+            database.execSQL("CREATE UNIQUE INDEX `index_user_uuid` ON `user` (`uuid`)");
+            database.endTransaction();
         }
     };
 
