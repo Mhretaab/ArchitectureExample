14ad4920fdf204744683b921c556b5f8ddd72e48
Mhretaab <ermu96@gmail.com>
2019-09-22 18:01:41 +0300

    mig script for creating note_detail view

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -11,11 +11,10 @@ public final class MigrationsHelper {
 
             database.execSQL("CREATE TABLE `user` (`first_name` TEXT, `last_name` TEXT, `email` TEXT, `password` TEXT, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, `street` TEXT, `state` TEXT, `city` TEXT, `post_code` INTEGER, FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
 
-            database.execSQL("ALTER TABLE `note` RENAME TO `note_old`");
             database.execSQL("CREATE TABLE `note` (`title` TEXT, `description` TEXT, `priority` INTEGER NOT NULL, `user_id` INTEGER, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
             database.execSQL("INSERT INTO `note`(`title`, `description`, `priority`, `id`, `uuid`, `date_created`, `date_updated`)\n" +
                     "SELECT `title`, `description`, `priority`, `id`, `uuid`, 1569163537, 1569163537 \n" +
-                    "FROM note_old");
+                    "FROM note_table");
             database.execSQL("DROP TABLE `note_old`");
 
             database.execSQL("CREATE INDEX `index_note_title` ON `note` (`title`)");
