5228c32df4db4c267e35e326596427f46189e04e
Mhretaab <ermu96@gmail.com>
2019-09-22 17:46:53 +0300

    added default value for dates

--- app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
+++ app/src/main/java/com/example/architectureexample/common/db/MigrationsHelper.java
@@ -16,8 +16,8 @@ public final class MigrationsHelper {
 
             database.execSQL("ALTER TABLE `note` RENAME TO `note_old`");
             database.execSQL("CREATE TABLE `note` (`title` TEXT, `description` TEXT, `priority` INTEGER NOT NULL, `user_id` INTEGER, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `uuid` TEXT NOT NULL, `date_created` INTEGER NOT NULL, `date_updated` INTEGER NOT NULL, `created_by_user_id` INTEGER, `updated_by_user_id` INTEGER, FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`created_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION , FOREIGN KEY(`updated_by_user_id`) REFERENCES `user`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )");
-            database.execSQL("INSERT INTO `note`(`title`, `description`, `priority`, `id`, `uuid`)\n" +
-                    "SELECT `title`, `description`, `priority`, `id`, `uuid` \n" +
+            database.execSQL("INSERT INTO `note`(`title`, `description`, `priority`, `id`, `uuid`, `date_created`, `date_updated`)\n" +
+                    "SELECT `title`, `description`, `priority`, `id`, `uuid`, 1569163537, 1569163537 \n" +
                     "FROM note_old");
             database.execSQL("DROP TABLE `note_old`");
 
